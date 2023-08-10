89aeece93f08b3135cbe96ce2961aaf64333bc09
ashish-krishna-k <ashishk.krishna@gmail.com>
2023-08-10 21:56:09 +0530

    Refactors few methods.

--- src/todo-list.ts
+++ src/todo-list.ts
@@ -1,15 +1,20 @@
+import EventsObserver from "./index";
+
 const TodoList = (() => {
     let _listArray: Project[] = [];
 
     const getList = () => _listArray.slice();
-    const addProjectToList = (project: Project) => _listArray.push(project);
-    const replaceList = (list: Project[]) => _listArray = list;
+    const addProjectToList = (project: Project) => {
+        _listArray.push(project);
+        saveListToLocalStorage();
+        EventsObserver.publish("addsProjectToList", getList());
+    };
     const saveListToLocalStorage = () => {
         window.localStorage.setItem("TodoList", JSON.stringify(getList()));
     }
     const loadListFromLocalStorage = () => {
-        const savedList = JSON.parse(window.localStorage.getItem("TodoList"));
-        replaceList(savedList);
+        _listArray = JSON.parse(window.localStorage.getItem("TodoList"));
+        EventsObserver.publish("listModified", getList());
     }
 
     return {
